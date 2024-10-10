63bc4a9383f19fcb1fcbdbfb469a325d91eb0dd5
spectre <e.sahinnn61@gmail.com>
2024-10-10 18:29:40 +0200

    api transactions bug

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -10,9 +10,9 @@ const SettingsPage = async () => {
           <img
             src={session.user.image || "/default-avatar.png"}
             alt="Profile"
-            className="w-16 h-16 rounded-full mb-2"
+            className="w-12 h-12 object-cover rounded-full mb-2"
           />
-          <p className="text-lg font-semibold">{session.user.name || "User"}</p>
+          <p className="text-md ">{session.user.name || "User"}</p>
           <p className="text-sm text-gray-500">{session.user.email}</p>
         </div>
       )}
@@ -24,9 +24,9 @@ const SettingsPage = async () => {
       >
         <button
           type="submit"
-          className="px-6 py-2 bg-red-500 text-white font-semibold rounded-md shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 transition duration-200 ease-in-out"
+          className="px-4 py-1 bg-red-500 text-white text-[12px] rounded-md shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 transition duration-200 ease-in-out"
         >
-          Sign out
+          Déconnexion
         </button>
       </form>
     </div>
---
+++ prisma/migrations/20241010154912_change_user_id_to_string/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - The primary key for the `Transaction` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Account" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Transaction_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
